8f4eeb20e488274a0e1741f4a318c73f5ed79c26
apiraino <gitlab@storiepvtride.it>
2018-12-29 21:00:08 +0100

    proof reading

--- main.js
+++ main.js
@@ -1,45 +1,45 @@
 const FEATURES = [
   {
     title: '`async` as a keyword in 2018 edition',
-    stablized: {
+    stabilized: {
       version: '1.28',
-      pr: '50307',
-    },
+      pr: '50307'
+    }
   },
   {
     title: '2018 edition',
-    stablized: {
+    stabilized: {
       version: '1.31',
-      pr: '54057',
-    },
+      pr: '54057'
+    }
   },
   {
     title: '`Pin` as a method receiver',
     rfc: '2362',
     tracking: '55786',
-    stablized: {
+    stabilized: {
       version: '1.33',
-      pr: '56805',
-    },
+      pr: '56805'
+    }
   },
   {
     title: 'Pin APIs',
     rfc: '2349-pin',
     tracking: '49150',
-    stablized: {
+    stabilized: {
       version: '1.33',
-      pr: '56939',
-    },
+      pr: '56939'
+    }
   },
   {
     title: '`std::task` and `std::future`',
-    rfc: '2592',
+    rfc: '2592'
   },
   {
     title: '`async`/`await` notation',
     rfc: '2394-async_await',
-    tracking: '50547',
-  },
+    tracking: '50547'
+  }
 ];
 
 // The following code is modified from forge.rust-lang.org
@@ -52,7 +52,7 @@ const stableMinorVersion = releases + epochRelease;
 const betaMinorVersion = releases + epochRelease + 1;
 
 const $features = document.getElementById('features');
-for (const { title, rfc, tracking, stablized } of FEATURES) {
+for (const { title, rfc, tracking, stabilized } of FEATURES) {
   const $li = $c('li');
   // Title
   $li.innerHTML = title.replace(
@@ -62,35 +62,35 @@ for (const { title, rfc, tracking, stablized } of FEATURES) {
   const appendText = text => $li.appendChild(document.createTextNode(text))
   // Stablization information
   appendText(' ');
-  if (!stablized) {
+  if (!stabilized) {
     $li.appendChild($c('span', {
-      className: 'not-stablized',
-      textContent: 'not stablized yet',
+      className: 'not-stabilized',
+      textContent: 'not stabilized yet'
     }));
   } else {
-    const { version, pr } = stablized;
+    const { version, pr } = stabilized;
     $li.appendChild($c('a', {
-      className: 'stablized',
-      textContent: `stablized in ${version}`,
+      className: 'stabilized',
+      textContent: `stabilized in ${version}`,
       href: `https://github.com/rust-lang/rust/pull/${pr}`,
-      target: '_blank',
+      target: '_blank'
     }));
     appendText(' ');
     const [_, minor] = version.split('.').map(n => parseInt(n, 10));
     if (minor <= stableMinorVersion) {
       $li.appendChild($c('span', {
         className: 'stable',
-        textContent: '[in stable]',
+        textContent: '[in stable]'
       }));
     } else if (minor == betaMinorVersion) {
       $li.appendChild($c('span', {
         className: 'beta',
-        textContent: '[in beta]',
+        textContent: '[in beta]'
       }));
     } else {
       $li.appendChild($c('span', {
         className: 'nightly',
-        textContent: '[in nightly]',
+        textContent: '[in nightly]'
       }));
     }
   }
@@ -101,7 +101,7 @@ for (const { title, rfc, tracking, stablized } of FEATURES) {
   if (rfc) {
     const $rfc = $c('a', {
       className: 'rfc',
-      target: '_blank',
+      target: '_blank'
     });
     const dash = rfc.indexOf('-');
     if (dash === -1) {
@@ -124,7 +124,7 @@ for (const { title, rfc, tracking, stablized } of FEATURES) {
       href: `https://github.com/rust-lang/rust/issues/${tracking}`,
       textContent: `#${tracking}`,
       title: 'Tracking issue',
-      target: '_blank',
+      target: '_blank'
     }));
   }
   $features.insertBefore($li, $features.firstChild);
@@ -135,5 +135,5 @@ function $c(tag, props = {}) {
   for (const prop in props) {
     elem[prop] = props[prop];
   }
-  return elem;
+  return elem
 }

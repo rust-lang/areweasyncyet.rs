b850c61466a7fe03a49dcc4e19fce1420e3c93a7
Xidorn Quan <me@upsuper.org>
2019-01-08 18:05:44 +1100

    adjust order

--- main.js
+++ main.js
@@ -41,6 +41,10 @@ const ASYNC_BLOCKERS = [
     },
   },
   {
+    title: 'async fn should support multiple lifetimes',
+    tracking: '56238',
+  },
+  {
     title: '`std::task` and `std::future`',
     rfc: '2592',
   },
@@ -48,10 +52,6 @@ const ASYNC_BLOCKERS = [
     title: '`async`/`await` syntax',
     rfc: '2394-async_await',
     tracking: '50547',
-  },
-  {
-    title: 'async fn should support multiple lifetimes',
-    tracking: '56238',
   },
 ];
 

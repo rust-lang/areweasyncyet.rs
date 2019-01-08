ecff7a6104d5da36bc4b56c7a8e3208ff3010bbe
Philip Jenvey <pjenvey@underboss.org>
2019-01-07 23:06:40 -0800

    Include issue #56238 (#5)

--- main.js
+++ main.js
@@ -41,6 +41,10 @@ const ASYNC_BLOCKERS = [
     },
   },
   {
+    title: 'async fn should support multiple lifetimes',
+    tracking: '56238',
+  },
+  {
     title: '`std::task` and `std::future`',
     rfc: '2592',
   },

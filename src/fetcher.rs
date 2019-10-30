ebd02a19a2230fcaead2dc291fa65ee03ead72dd
Xidorn Quan <xidorn@canva.com>
2019-10-30 19:11:10 +1100

    Fetch issues concurrently with async

--- src/fetcher.rs
+++ src/fetcher.rs
@@ -1,6 +1,9 @@
 use crate::data::input::FetchList;
 use crate::data::{Issue, IssueId};
 use crate::query::{GitHubQuery, Repo};
+use futures_util::future::ok;
+use futures_util::stream::FuturesUnordered;
+use futures_util::try_stream::TryStreamExt;
 use serde::{Deserialize, Serialize};
 use serde_with::rust::hashmap_as_tuple_list;
 use std::collections::HashMap;
@@ -38,32 +41,59 @@ impl IssueData {
         fetch_list: &FetchList<'_>,
     ) -> Result<bool, Box<dyn Error>> {
         let mut updated = false;
-        for (repo, label) in fetch_list.labels.iter() {
-            let key = (repo.clone(), label.to_string());
-            if self.labels.contains_key(&key) {
-                continue;
-            }
-            let issues = query.query_issues_with_label(repo, label).await?;
-            let issues = issues
-                .iter()
-                .map(|issue| {
-                    let id = issue.number;
-                    self.issues.insert((repo.clone(), id), issue.clone());
-                    id
-                })
-                .collect();
-            self.labels.insert(key, issues);
-            updated = true;
-        }
-        for (repo, issue_id) in fetch_list.issues.iter() {
-            let key = (repo.clone(), *issue_id);
-            if self.issues.contains_key(&key) {
-                continue;
-            }
-            let issue = query.query_issue_or_pr(repo, *issue_id).await?;
-            self.issues.insert(key, issue);
-            updated = true;
-        }
+        fetch_list
+            .labels
+            .iter()
+            .filter_map(|(repo, label)| {
+                let key = (repo.clone(), label.to_string());
+                if self.labels.contains_key(&key) {
+                    None
+                } else {
+                    Some(async {
+                        let (repo, label) = &key;
+                        let issues = query.query_issues_with_label(repo, label).await?;
+                        Ok::<_, Box<dyn Error>>((key, issues))
+                    })
+                }
+            })
+            .collect::<FuturesUnordered<_>>()
+            .try_for_each_concurrent(None, |(key, issues)| {
+                let (repo, _) = &key;
+                let issues = issues
+                    .into_iter()
+                    .map(|issue| {
+                        let id = issue.number;
+                        self.issues.insert((repo.clone(), id), issue);
+                        id
+                    })
+                    .collect();
+                self.labels.insert(key, issues);
+                updated = true;
+                ok(())
+            }).await?;
+
+        fetch_list
+            .issues
+            .iter()
+            .filter_map(|(repo, issue_id)| {
+                let key = (repo.clone(), *issue_id);
+                if self.issues.contains_key(&key) {
+                    None
+                } else {
+                    Some(async {
+                        let (repo, issue_id) = &key;
+                        let issue = query.query_issue_or_pr(repo, *issue_id).await?;
+                        Ok::<_, Box<dyn Error>>((key, issue))
+                    })
+                }
+            })
+            .collect::<FuturesUnordered<_>>()
+            .try_for_each_concurrent(None, |(key, issue)| {
+                self.issues.insert(key, issue);
+                updated = true;
+                ok(())
+            }).await?;
+
         Ok(updated)
     }
 }
